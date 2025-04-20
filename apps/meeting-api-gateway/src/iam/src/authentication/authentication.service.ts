475fd4215e2b40b913322618c95210db894db8b2
exelimpichment <oldestspy@gmail.com>
2025-04-20 18:33:48 +0200

    Refactor sign-in logic in AuthenticationService: restore and enhance user sign-in functionality with error handling for non-existent users and password mismatches. Update module imports to streamline repository access.

--- apps/meeting-api-gateway/src/iam/src/authentication/authentication.service.ts
+++ apps/meeting-api-gateway/src/iam/src/authentication/authentication.service.ts
@@ -1,5 +1,5 @@
+import { UsersRepository } from '@apps/meeting-api-gateway/src/iam/src/users/repositories';
 import { handlePrismaError } from '@apps/meeting-api-gateway/src/iam/src/common/utils';
-import { UsersRepository } from '../users/repositories/users.repository';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { HashingService } from '@libs/hashing/src';
 import { SignInDto } from './dto/sign-in.dto';
@@ -10,25 +10,6 @@ export class AuthenticationService {
     private readonly usersRepository: UsersRepository,
     private readonly hashingService: HashingService,
   ) {}
-
-  async signIn(signInDto: SignInDto): Promise<boolean> {
-    const user = await this.usersRepository.findOneByEmail(signInDto.email);
-
-    if (!user) {
-      throw new UnauthorizedException('User does not exist');
-    }
-
-    const isMatch = await this.hashingService.compare(
-      signInDto.password,
-      user.password,
-    );
-
-    if (!isMatch) {
-      throw new UnauthorizedException('Password does not match');
-    }
-
-    return true;
-  }
 
   async signUp(signInDto: SignInDto) {
     const email = signInDto.email;
@@ -49,5 +30,24 @@ export class AuthenticationService {
     } catch (error) {
       handlePrismaError(error);
     }
+  }
+
+  async signIn(signInDto: SignInDto): Promise<boolean> {
+    const user = await this.usersRepository.findOneByEmail(signInDto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('User does not exist');
+    }
+
+    const isMatch = await this.hashingService.compare(
+      signInDto.password,
+      user.password,
+    );
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Password does not match');
+    }
+
+    return true;
   }
 }
