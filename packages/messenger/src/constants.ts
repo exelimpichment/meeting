96d75d4c9988cb104a01ead6da5e63bdcb5d8dff
exelimpichment <oldestspy@gmail.com>
2025-10-11 15:47:16 +0200

    Remove meeting and messenger packages along with their configurations and documentation. Update package.json and pnpm-lock.yaml to include new dependency @exelimpichment/prisma-types and restore pino and pino-pretty dependencies. This cleanup streamlines the project structure and prepares for future enhancements.

--- packages/messenger/src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const MessageEventType = {
-  SEND: 'message.send',
-  EDIT: 'message.edit',
-  DELETE: 'message.delete',
-} as const;
--- packages/messenger/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-// re-export Prisma types with custom names
-export { PrismaClient as MessengerPrismaClient } from '../generated/index';
-
-// export custom types
-export type { ApiResponses } from './types';
-
-export { MessageEventType } from './constants';
