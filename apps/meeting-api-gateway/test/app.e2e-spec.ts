3ae58e280136fc1638df6f5674a0ea21383b51b6
exelimpichment <oldestspy@gmail.com>
2025-11-26 19:09:14 +0100

    feat: add Docker Compose configuration and e2e test enhancements
    - Introduced a new Docker Compose file for setting up a MySQL test environment.
    - Added new scripts in package.json for managing the test environment and database migrations.
    - Implemented an e2e test endpoint in MeetingApiGatewayController to return a simple message.
    - Enhanced e2e tests to mock NATS and Kafka clients, ensuring no actual connections are attempted during tests.
    - Updated jest-e2e configuration for improved module resolution and test organization.

--- apps/meeting-api-gateway/test/app.e2e-spec.ts
+++ apps/meeting-api-gateway/test/app.e2e-spec.ts
@@ -2,6 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { MeetingApiGatewayModule } from './../src/meeting-api-gateway.module';
+import {
+  MEETING_API_NATS_CLIENT,
+  MEETING_API_KAFKA_CLIENT,
+} from '@/apps/meeting-api-gateway/src/constants';
 
 describe('MeetingApiGatewayController (e2e)', () => {
   let app: INestApplication;
@@ -9,16 +13,37 @@ describe('MeetingApiGatewayController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [MeetingApiGatewayModule],
-    }).compile();
+    })
+      // mock nats client to avoid connection attempts
+      .overrideProvider(MEETING_API_NATS_CLIENT)
+      .useValue({
+        emit: jest.fn(),
+        send: jest.fn(),
+        connect: jest.fn(),
+        close: jest.fn(),
+      })
+      // mock kafka client
+      .overrideProvider(MEETING_API_KAFKA_CLIENT)
+      .useValue({
+        emit: jest.fn(),
+        send: jest.fn(),
+        connect: jest.fn(),
+        close: jest.fn(),
+      })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('/e2e-test (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/e2e-test')
       .expect(200)
-      .expect('Hello World!');
+      .expect({ message: 'Hello World!' });
   });
 });
